f6a213c04f85f53fc8654e0973f6261c7356f13d
Gull 2365 <jych2365@gmail.com>
2023-08-20 22:49:18 +0900

    feat: static rendering, dynamic rendering, cache

--- app/list/page.js
+++ app/list/page.js
@@ -1,7 +1,7 @@
 import { connectDB } from "@/util/database";
-import Link from "next/link";
-import DetailLink from "./DetailLink";
 import ListItem from "./ListItem";
+
+export const dynamic = "force-dynamic";
 
 export default async function List() {
   const db = (await connectDB).db("board");
